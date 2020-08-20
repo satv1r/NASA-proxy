5a3910809f0004ed16fd3382004e14bdec3cf596
Satvir Sandhu <356Sandhu@gmail.com>
2020-08-19 20:39:11 -0400

    add weather endpoint

--- src/api/mars-weather.js
+++ src/api/mars-weather.js
@@ -1,9 +1,26 @@
 const express = require("express");
+const axios = require("axios");
 
 const router = express.Router();
+// https://api.nasa.gov/planetary/apod?api_key=o50slHLqJd3LnaWMAegD0nN5q83KIAw7CicARCKX
+const BASE_URL = "https://api.nasa.gov/insight_weather/?";
 
-router.get("/", (req, res) => {
-  res.json(["😀", "😳", "🙄"]);
+router.get("/", async (req, res, next) => {
+  try {
+    const params = new URLSearchParams({
+      api_key: process.env.NASA_KEY,
+      feedtype: "json",
+      ver: "1.0",
+    });
+    console.log(`${BASE_URL}${params}`);
+    // 1. make a request to nasa api
+    const { data } = await axios.get(`${BASE_URL}${params}`);
+    console.log(data);
+    // 2. respond to this request with data from the NASA api
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
