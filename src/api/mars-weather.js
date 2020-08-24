ff42c2ad74232800c3c92682a3ae863c2b79d73d
Satvir Sandhu <356Sandhu@gmail.com>
2020-08-24 11:31:59 -0400

    add api keys

--- src/api/mars-weather.js
+++ src/api/mars-weather.js
@@ -12,10 +12,6 @@ const speedLimiter = slowDown({
   windowMs: 30 * 1000, // 30 seconds
   delayAfter: 1, // allow 100 requests per 15 minutes, then...
   delayMs: 500, // begin adding 500ms of delay per request above 100:
-  // request # 101 is delayed by  500ms
-  // request # 102 is delayed by 1000ms
-  // request # 103 is delayed by 1500ms
-  // etc.
 });
 
 const router = express.Router();
@@ -25,28 +21,45 @@ const BASE_URL = "https://api.nasa.gov/insight_weather/?";
 let cachedData;
 let cacheTime;
 
-router.get("/", limiter, speedLimiter, async (req, res, next) => {
-  // In memory cache
-  if (cacheTime && cacheTime > Date.now() - 60 * 15 * 1000) {
-    return res.json(cachedData);
+const apiKeys = new Map();
+apiKeys.set("12345", true);
+
+router.get(
+  "/",
+  limiter,
+  speedLimiter,
+  (req, res, next) => {
+    const apiKey = req.get("X-API-KEY");
+    if (apiKeys.has(apiKey)) {
+      next();
+    } else {
+      const error = new Error("Invalid API Key");
+      next(error);
+    }
+  },
+  async (req, res, next) => {
+    // In memory cache
+    if (cacheTime && cacheTime > Date.now() - 60 * 15 * 1000) {
+      return res.json(cachedData);
+    }
+    try {
+      const params = new URLSearchParams({
+        api_key: process.env.NASA_KEY,
+        feedtype: "json",
+        ver: "1.0",
+      });
+      // 1. make a request to nasa api
+      const { data } = await axios.get(`${BASE_URL}${params}`);
+      // 2. store data in cache and update cache time
+      cachedData = data;
+      cacheTime = Date.now();
+      data.cacheTime = cacheTime;
+      // 3. respond to request with data
+      return res.json(data);
+    } catch (error) {
+      return next(error);
+    }
   }
-  try {
-    const params = new URLSearchParams({
-      api_key: process.env.NASA_KEY,
-      feedtype: "json",
-      ver: "1.0",
-    });
-    // 1. make a request to nasa api
-    const { data } = await axios.get(`${BASE_URL}${params}`);
-    // 2. store data in cache and update cache time
-    cachedData = data;
-    cacheTime = Date.now();
-    data.cacheTime = cacheTime;
-    // 3. respond to request with data
-    return res.json(data);
-  } catch (error) {
-    return next(error);
-  }
-});
+);
 
 module.exports = router;
